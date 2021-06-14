1039591d8321e37764f3ab092601f24567419adb
Simonsen94 <soerenkristiansimonsen@gmail.com>
2021-06-14 17:01:09 +0200

    update form + add map

--- src/components/Coffeeform.js
+++ src/components/Coffeeform.js
@@ -13,36 +13,36 @@ export default function CoffeeForm() {
                     value={value}
                     onChange={e => setValue(e.target.value)}
                 />
-                <p>Enter your name</p>
-                <input
-                    type='text'
-                />
-                <p>Select roast level</p>
+                <p>How do you typically like your cofffee to be roasted?</p>
                 <select>
                     <option value="1">Light</option>
-                    <option value="2">Medium-Light</option>
+                    <option value="2">Medium light</option>
                     <option value="3">Medium</option>
-                    <option value="4">Medium-Dark</option>
+                    <option value="4">Medium dark</option>
                     <option value="5">Dark</option>
+                    <option value="6">Very dark</option>
                 </select>
                 <p>How do you like your coffee to taste?</p>
                 <select>
-                    <option value="1">I like a classic and traditional coffee taste.</option>
-                    <option value="2">I like the notes to have hints of something different.</option>
-                    <option value="3">I like to have a taste explosion of different notes.</option>
+                    <option value="1">I like a classic and traditional coffee taste</option>
+                    <option value="2">I like the notes to have hints of something different</option>
+                    <option value="3">I like to have a taste explosion of different notes</option>
+                </select>
+                <p>Do you add anything to your coffee?</p>
+                <select>
+                    <option value="1">I take it black</option>
+                    <option value="2">I like to add milk or cream</option>
                 </select>
                 <p>Select your top 3 notes</p>
                 <input 
                     type='text'
                 />
-                <p>Select price range</p>
+                <p>What type of coffee do you drink?</p>
                 <select>
-                    <option value="1">10-20$</option>
-                    <option value="2">20-30$</option>
-                    <option value="3">30-50$</option>
-                    <option value="4">+50$</option>
+                    <option value="1">Original</option>
+                    <option value="2">Decaf</option>
                 </select>
-                <p>Submit your answers and let our AI do its magic.</p>
+                <p>Submit your answers and find your perfect match.</p>
                 <button>Submit</button>
             </form>
         </div> 
--- src/pages/localCafe.js
+++ src/pages/localCafe.js
@@ -6,8 +6,13 @@ export default function localCafe() {
     return (
         <Layout>
             <div className={styles.portfolio}>
-                <h2>Copenhagen Coffee Cruise</h2>
-                <h4>Where to find Your next great coffee</h4>
+                <h2>Specialty coffee roasters</h2>
+                <div>
+                    <iframe title="coffeemap" class={styles.googlemap} src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d9000.
+                    579919774576!2d12.6150629!3d55.669079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xa2c7ccb637f655ff!2sNordic%2
+                    0Roasting%20Co!5e0!3m2!1sen!2sdk!4v1623486558162!5m2!1sen!2sdk" width="800" height="600" frameborder="0" 
+                    style={{border:0}} allowfullscreen="" aria-hidden="false"></iframe>
+                </div>
             </div>
         </Layout>
     )
