76ee13d14fba2c508a25003d633b81ff35c6ae95
Simonsen94 <soerenkristiansimonsen@gmail.com>
2021-09-26 18:13:57 +0200

    implementet GA4 to site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,27 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'BeanBusters',
+    description: 'Discover unique coffee',
+    copyright: 'This website is copyright 2021 BeanBusters',
+    contact: 'sks@beanbusters.com'
+  },
   plugins: [
     'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-transformer-json',
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: "G-G3YBDHWJ4S",
+        // this option places the tracking script into the head of the DOM
+        head: true,
+        // other options
+        anonymize: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -33,10 +49,4 @@ module.exports = {
       },
     },
   ],
-  siteMetadata: {
-    title: 'BeanBusters',
-    description: 'Discover unique coffee',
-    copyright: 'This website is copyright 2021 BeanBusters',
-    contact: 'sks@beanbusters.com'
-  },
 }
